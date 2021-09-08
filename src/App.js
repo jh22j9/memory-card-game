de6a35f458ee9acbf0e85433ef04980191cd9dac
jheeju <jheeju.dev@gmail.com>
2021-09-08 10:16:26 +0900

    :construction: Add card images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import "./App.css";
+
 function App() {
+  const images = ["01", "02", "03", "04", "05", "06"].map(
+    (el) => `img/${el}.png`
+  );
+
+  const pairOfCards = [...images, ...images];
+
   return (
-    <div className="App">
-      Memory card game
-    </div>
+    <section className="memory-game">
+      {pairOfCards.map((image, index) => {
+        return (
+          <div className="memory-card" key={index}>
+            <img className="front-face" src={image} alt={index} />
+            <h3 className="back-face">Click!</h3>
+          </div>
+        );
+      })}
+    </section>
   );
 }
 
